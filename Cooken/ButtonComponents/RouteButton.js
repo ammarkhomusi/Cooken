30b1483be6bed8340affb493ca008b4193c0cf9b
Tyler Wells <tyler.f.wells@gmail.com>
2022-05-03 16:35:52 -0300

    tyler adding in his branch
    test of us trying to merge branches while on an HR with Juan

--- Cooken/ButtonComponents/RouteButton.js
+++ Cooken/ButtonComponents/RouteButton.js
@@ -1,31 +1,29 @@
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import React from "react";
 
 export default function RouteButton({ text, onPress }) {
   const content = (
     <View style={styles.button}>
       <Text style={styles.text}>{text}</Text>
     </View>
-  )
-  return (
-    <TouchableOpacity onPress={onPress}>{content}</TouchableOpacity>
-  )
+  );
+  return <TouchableOpacity onPress={onPress}>{content}</TouchableOpacity>;
 }
 
 const styles = StyleSheet.create({
   button: {
-    backgroundColor:'#F6A84C',
-    flexDirection: 'row',
-    justifyContent:'flex-start',
-    alignItems:'flex-start',
-    marginBottom:30,
+    backgroundColor: "#F6A84C",
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+    marginBottom: 30,
 
     height: 62,
-    width:20,
-    borderRadius: 100/ 5,
+    width: 20,
+    borderRadius: 100 / 5,
   },
   text: {
-    color: 'white',
-    fontSize: 20
-  }
-})
+    color: "white",
+    fontSize: 20,
+  },
+});
